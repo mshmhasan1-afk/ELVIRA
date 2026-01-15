b06ae6f077e2e035fd7638d707deb33d2dd67032
mshmhasan1-afk <mshmhasan1@gmail.com>
2026-01-15 19:58:05 +0500

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,6 @@
+// Firebase config + auth + Firestore + Storage will be added here
+// Pseudo:
+  // 1. Login email/password
+  // 2. Show admin panel
+  // 3. Upload image to storage
+  // 4. Add product to Firestore
